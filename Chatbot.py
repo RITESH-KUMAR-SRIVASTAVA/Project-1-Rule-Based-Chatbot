74f9c9905ec6a9e9f4c39f2af213ac17e21e58c3
Ritesh Kumar Srivastava <ritesh18sri@gmail.com>
2026-09-23 18:38:13 +0530

    Create rule-based AI chatbot

---
+++ Chatbot.py
@@ -0,0 +1,32 @@
+# DecodeLabs - Project 1
+
+# This is a simple rule-based AI chatbot implemented in Python. It responds to user inputs based on predefined rules.
+#You can enter greetings(hello, hi, hey), ask how are you, what is your name, ask for help, express gratitude(thanks, thank you), or ask what is AI. The chatbot will respond accordingly. To exit the conversation, type 'bye', 'exit', or 'quit'.
+print("RChat: Hello! I am RChat, your Rule-Based AI Chatbot.");
+print("RChat: Type 'help' to see what you can ask me.");
+
+responses = {
+    "hello": "Hello! How can I help you?",
+    "hi": "Hello! How can I help you?",
+    "hey": "Hello! How can I help you?",
+
+    "how are you": "I'm doing great! Thank you for asking.",
+
+    "what is your name": "I am a Rule-Based AI Chatbot created by Ritesh Srivastava.",
+
+    "help": "You can enter greetings(hello, hi, hey), ask how are you, what is your name, express gratitude(thanks, thank you), or ask what is AI.",
+
+    "thanks": "You're welcome!",
+    "thank you": "You're welcome!",
+    "what is ai": "AI stands for Artificial Intelligence. It enables computers to perform tasks that normally require human intelligence.",
+}
+while True:
+    raw_input = input('You: ');
+    clean_input = raw_input.lower().strip();
+
+    if clean_input in ["bye", "exit", "quit"]:
+        print("RChat: Goodbye! Have a nice day.");
+        break;
+
+    reply = responses.get(clean_input, "I don't understand.")
+    print("RChat: " + reply);
